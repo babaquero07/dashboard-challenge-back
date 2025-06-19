34c76b6350eb012ced6c5c5e2c06e0d4caadcf37
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-19 17:59:43 -0500

    feat(db-connection): Integrate TypeORM with MySQL support in NestJS application.  Configured TypeOrmModule in AppModule for database connection settings.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 3306,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // Change to false in production
+    }),
+  ],
 })
 export class AppModule {}
