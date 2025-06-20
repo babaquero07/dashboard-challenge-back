4392d2d983e9ca9db523619a6364b51a15fd30de
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-19 20:35:46 -0500

    refactor(auth): Update LocalStrategy to use UsersService for user validation and modify payload structure

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,19 +1,22 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from '../auth.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private usersService: UsersService) {
     super();
   }
 
-  async validate(email: string, password: string) {
+  async validate(payload: { id: number; email: string }) {
     console.log('inside local strategy');
-    const user = await this.authService.login({ email, password });
-    if (!user) throw new UnauthorizedException();
+    const user = await this.usersService.findByEmail(payload.email);
+    if (!user) throw new UnauthorizedException('invalid token');
 
-    return user;
+    return {
+      id: user.id,
+      email: user.email,
+    };
   }
 }
