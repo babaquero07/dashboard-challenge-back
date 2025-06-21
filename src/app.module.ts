682b6e1d3d6aeb2cd4f812e858c8d6d6eacd840d
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-20 19:38:48 -0500

    feat(app-module): Integrate DashboardsModule into AppModule for dashboard functionality; add GetUser decorator for user retrieval in requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DashboardsModule } from './dashboards/dashboards.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    DashboardsModule,
   ],
 })
 export class AppModule {}
